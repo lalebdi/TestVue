99b50d528b60e119e9afa3f34c90189ad6a8ad94
lalebdi <loai.alebdi@gmail.com>
2021-04-01 15:29:14 -0400

    initial nav bar styling

--- first-vue/src/components/TopHeader.vue
+++ first-vue/src/components/TopHeader.vue
@@ -7,8 +7,8 @@
 
         <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-            <b-nav-item href="#">Link</b-nav-item>
-            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to="/about">About</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
@@ -18,21 +18,13 @@
             <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
             </b-nav-form>
 
-            <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-            </b-nav-item-dropdown>
+            <!-- <b-nav-item-dropdown text="number of items" right>
+            <b-dropdown-item href="#">10</b-dropdown-item>
+            <b-dropdown-item href="#">20</b-dropdown-item>
+            <b-dropdown-item href="#">30</b-dropdown-item>
+            <b-dropdown-item href="#">40</b-dropdown-item>
+            </b-nav-item-dropdown> -->
 
-            <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template #button-content>
-                <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-            </b-nav-item-dropdown>
         </b-navbar-nav>
         </b-collapse>
     </b-navbar>
@@ -46,5 +38,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#nav a.router-link-exact-active{
+    color: white;
+}
 
 </style>
