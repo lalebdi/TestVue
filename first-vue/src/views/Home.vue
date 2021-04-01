4d8057cf52c55ee7f8d6a5c9638744d0da497145
lalebdi <loai.alebdi@gmail.com>
2021-03-31 23:13:18 -0400

    create cards and responsive

--- first-vue/src/views/Home.vue
+++ first-vue/src/views/Home.vue
@@ -1,7 +1,34 @@
 <template>
   <b-container>
     <b-row align-v="center" >
-      <b-col>
+      <b-col md="3">
+        <b-card title="This is a card" img-src="https://unsplash.com/photos/M0WbGFRTXqU" img-alt="Fire safety Product" img-top tag="article" style="max-width:20rem" class="mb-2">
+          <b-card-text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, minus?
+          </b-card-text>
+          <b-button variant="primary">More info</b-button>
+        </b-card>
+      </b-col>
+
+      <b-col md="3">
+        <b-card title="This is a card" img-src="https://unsplash.com/photos/M0WbGFRTXqU" img-alt="Fire safety Product" img-top tag="article" style="max-width:20rem" class="mb-2">
+          <b-card-text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, minus?
+          </b-card-text>
+          <b-button variant="primary">More info</b-button>
+        </b-card>
+      </b-col>
+
+      <b-col md="3">
+        <b-card title="This is a card" img-src="https://unsplash.com/photos/M0WbGFRTXqU" img-alt="Fire safety Product" img-top tag="article" style="max-width:20rem" class="mb-2">
+          <b-card-text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, minus?
+          </b-card-text>
+          <b-button variant="primary">More info</b-button>
+        </b-card>
+      </b-col>
+
+      <b-col md="3">
         <b-card title="This is a card" img-src="https://unsplash.com/photos/M0WbGFRTXqU" img-alt="Fire safety Product" img-top tag="article" style="max-width:20rem" class="mb-2">
           <b-card-text>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, minus?
