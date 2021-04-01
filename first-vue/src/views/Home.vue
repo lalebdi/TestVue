5d3bcc4212467d684e167b686cddb2c78bd5e5d4
lalebdi <loai.alebdi@gmail.com>
2021-04-01 09:54:36 -0400

    render product card

--- first-vue/src/views/Home.vue
+++ first-vue/src/views/Home.vue
@@ -1,53 +1,23 @@
 <template>
   <b-container>
     <b-row align-v="center" >
-      <b-col md="3">
-        <b-card title="This is a card" img-src="https://unsplash.com/photos/M0WbGFRTXqU" img-alt="Fire safety Product" img-top tag="article" style="max-width:20rem" class="mb-2">
-          <b-card-text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, minus?
-          </b-card-text>
-          <b-button variant="primary">More info</b-button>
-        </b-card>
-      </b-col>
-
-      <b-col md="3">
-        <b-card title="This is a card" img-src="https://unsplash.com/photos/M0WbGFRTXqU" img-alt="Fire safety Product" img-top tag="article" style="max-width:20rem" class="mb-2">
-          <b-card-text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, minus?
-          </b-card-text>
-          <b-button variant="primary">More info</b-button>
-        </b-card>
-      </b-col>
-
-      <b-col md="3">
-        <b-card title="This is a card" img-src="https://unsplash.com/photos/M0WbGFRTXqU" img-alt="Fire safety Product" img-top tag="article" style="max-width:20rem" class="mb-2">
-          <b-card-text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, minus?
-          </b-card-text>
-          <b-button variant="primary">More info</b-button>
-        </b-card>
-      </b-col>
-
-      <b-col md="3">
-        <b-card title="This is a card" img-src="https://unsplash.com/photos/M0WbGFRTXqU" img-alt="Fire safety Product" img-top tag="article" style="max-width:20rem" class="mb-2">
-          <b-card-text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, minus?
-          </b-card-text>
-          <b-button variant="primary">More info</b-button>
-        </b-card>
-      </b-col>
+      <product-card></product-card>
+      <product-card></product-card>
+      <product-card></product-card>
+      <product-card></product-card>
     </b-row>
   </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
-
+// import ProductCard from '@/components/ProductCard.vue'
+import ProductCard from '../components/ProductCard.vue'
 
 export default {
   name: 'Home',
   components: {
-
+    'product-card' : ProductCard
   },
 }
 </script>
