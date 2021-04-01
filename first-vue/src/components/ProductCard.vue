cab818607d6ecfb8f57cbf10c7f968c7847fc0c3
lalebdi <loai.alebdi@gmail.com>
2021-04-01 09:55:00 -0400

    make cards responsive

--- first-vue/src/components/ProductCard.vue
+++ first-vue/src/components/ProductCard.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-      <b-col md="3">
-        <b-card title="This is a card" img-src="https://unsplash.com/photos/M0WbGFRTXqU" img-alt="Fire safety Product" img-top tag="article" style="max-width:20rem" class="mb-2">
-          <b-card-text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, minus?
-          </b-card-text>
-          <b-button variant="primary">More info</b-button>
-        </b-card>
-      </b-col>
-  </div>
+    <div>
+        <b-col>
+            <b-card title="This is a card" img-src="https://unsplash.com/photos/M0WbGFRTXqU" img-alt="Fire safety Product" img-top tag="article" style="max-width:20rem" class="mb-2">
+            <b-card-text>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, minus?
+            </b-card-text>
+            <b-button variant="primary">More info</b-button>
+            </b-card>
+        </b-col>
+    </div>
 </template>
 
 <script>
