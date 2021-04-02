c0155475e32ff76a46a1a95fcf2d3e625ca5507e
lalebdi <loai.alebdi@gmail.com>
2021-04-01 23:00:27 -0400

    add a tool tip

--- first-vue/src/components/ProductCard.vue
+++ first-vue/src/components/ProductCard.vue
@@ -5,7 +5,7 @@
             <b-card-text>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, minus?
             </b-card-text>
-            <b-button variant="primary">More info</b-button>
+            <b-button variant="primary" v-b-tooltip.hover title="More about the product">More info</b-button>
             </b-card>
         </b-col>
     </div>
