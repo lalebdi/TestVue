e3070b4835a1a38781322b92891c64e28acac2de
lalebdi <loai.alebdi@gmail.com>
2021-04-01 21:41:08 -0400

    set up vuex

--- first-vue/src/store/index.js
+++ first-vue/src/store/index.js
@@ -4,8 +4,48 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    products: [],
+    displayProducts: [],
+    rows: 1,
+  },
+  mutations: {
+    SET_PRODUCTS(state, products){
+      state.products = products;
+    },
+    SET_ROWS(state, rows){
+      state.rows = rows;
+    },
+    SET_DISPLAY_PRODUCTS(state, displayProducts){
+      state.displayProducts = displayProducts;
+    }
+  },
+  actions: {
+    async fetchData(){
+      return new Promise(resolve => {
+        setTimeout(async () =>{
+          const res = await fetch("products.json");
+          const val = await res.json();
+          resolve(val);
+        }, 1000);
+      });
+    },
+    async fetchProducts({ dispatch, commit }){
+      const myJson = await dispatch("fetchData")
+      commit("SET_PRODUCTS", myJson);
+      commit("SET_ROWS", myJson.length);
+    }
+  },
+  getters: {
+    products(state){
+      return state.products;
+    },
+    displayProducts(state){
+      return state.displayProducts;
+    },
+    rows(state){
+      return state.rows;
+    },
+  },
   modules: {},
 })
